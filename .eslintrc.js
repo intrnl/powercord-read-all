60dac33fe5bea3bcdaa5f02ffa9500ef1958c709
intrnl <intrnl28+git@gmail.com>
2019-11-09 00:58:53 +0700

    Linting

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: '2018',
+  },
+
+  rules: {
+    'no-prototype-builtins': ['off'],
+
+    'use-isnan': ['error'],
+
+    'array-bracket-spacing': ['warn', 'never'],
+    'arrow-parens': ['warn', 'always'],
+    'arrow-spacing': ['warn', { before: true, after: true }],
+    'block-spacing': ['warn', 'always'],
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'dot-location': ['warn', 'property'],
+    'dot-notation': ['warn'],
+    'eol-last': ['warn', 'always'],
+    'func-call-spacing': ['warn', 'never'],
+    'indent': ['warn', 2, { SwitchCase: 1 }],
+    'key-spacing': ['warn', { beforeColon: false, afterColon: true }],
+    'keyword-spacing': ['warn', { before: true, after: true }],
+    'new-parens': ['warn', 'never'],
+    'newline-per-chained-call': ['warn', { ignoreChainWithDepth: 2 }],
+    'no-extra-boolean-cast': ['warn'],
+    'no-extra-semi': ['warn'],
+    'no-trailing-spaces': ['warn', { skipBlankLines: false, ignoreComments: true }],
+    'object-curly-spacing': ['warn', 'always',  { arraysInObjects: false, objectsInObjects: true }],
+    'prefer-regex-literals': ['warn'],
+    'quotes': ['warn', 'single'],
+    'semi-spacing': ['warn', { before: false, after: true }],
+    'semi': ['warn', 'always'],
+    'space-before-blocks': ['warn', 'always'],
+    'space-before-function-paren': ['warn', 'always'],
+    'switch-colon-spacing': ['warn', { after: true, before: false }],
+    'wrap-iife': ['warn', 'inside'],
+    'wrap-regex': ['warn'],
+  },
+};
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { Plugin } = require('powercord/entities')
-const { ContextMenu: { Button: CMButton } } = require('powercord/components')
-const { React, getModule, getModuleByDisplayName } = require('powercord/webpack')
-const { inject, uninject } = require('powercord/injector')
+const { Plugin } = require('powercord/entities');
+const { ContextMenu: { Button: CMButton } } = require('powercord/components');
+const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
 
 
 class ReadAll extends Plugin {
   startPlugin () {
-    this._patchContextMenu()
+    this._patchContextMenu();
   }
 
   pluginWillUnload () {
-    uninject('powercord-read-all_cm')
+    uninject('powercord-read-all_cm');
   }
 
   async _patchContextMenu () {
@@ -22,7 +22,7 @@ class ReadAll extends Plugin {
       getModuleByDisplayName('GuildContextMenu'),
       getModule(['getGuilds']),
       getModule(['markGuildAsRead']),
-    ])
+    ]);
 
     inject('powercord-read-all_cm', GuildContextMenu.prototype, 'render', (_, res) => {
       res.props.children[0].props.children = [
@@ -30,15 +30,15 @@ class ReadAll extends Plugin {
         React.createElement(CMButton, {
           name: 'Mark All As Read',
           onClick: () => {
-            const guilds = Object.keys(GuildStore.getGuilds())
-            GuildActions.markGuildsAsRead(guilds)
+            const guilds = Object.keys(GuildStore.getGuilds());
+            GuildActions.markGuildsAsRead(guilds);
           },
         }),
-      ]
+      ];
 
-      return res
-    })
+      return res;
+    });
   }
 }
 
-module.exports = ReadAll
+module.exports = ReadAll;
